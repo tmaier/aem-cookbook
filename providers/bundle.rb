32e89ed056dc7fc1d3a2db26c9fe9e0779050561
Tobias L. Maier <me@tobiasmaier.info>
2015-04-24 11:09:20 +0200

    Add budle resource and provider

---
+++ providers/bundle.rb
@@ -0,0 +1,131 @@
+#
+# Cookbook Name:: aem
+# Provider:: jcr_node_permission
+#
+# Copyright 2015
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This provider manages AEM bundles
+
+require 'json'
+
+def curl(url, user, password)
+  c = Curl::Easy.new(url)
+  c.http_auth_types = :basic
+  c.username = user
+  c.password = password
+  c.perform
+  c
+end
+
+def curl_form(url, user, password, fields)
+  c = Curl::Easy.http_post(url, *fields)
+  c.http_auth_types = :basic
+  c.username = user
+  c.password = password
+  c.multipart_form_post = true
+  c.perform
+  c
+end
+
+def curl_install(file, new_resource)
+  url = "http://#{new_resource[:host]}:#{new_resource[:port]}/system/console/bundles"
+  fields = [
+    Curl::PostField.content('action', 'install'),
+    Curl::PostField.content('bundlestartlevel', '20'),
+    Curl::PostField.content('bundlefile', %(@"#{file}")),
+  ]
+  curl_form(url, new_resource[:user], new_resource[:password], fields)
+end
+
+def curl_activate(new_resource)
+  url = "http://#{new_resource[:host]}:#{new_resource[:port]}/system/console/bundles/#{new_resource[:symbolic_name]}"
+  fields = [
+    Curl::PostField.content('action', 'start')
+  ]
+  curl_form(url, new_resource[:user], new_resource[:password], fields)
+end
+
+def curl_disable(new_resource)
+  url = "http://#{new_resource[:host]}:#{new_resource[:port]}/system/console/bundles/#{new_resource[:symbolic_name]}"
+  fields = [
+    Curl::PostField.content('action', 'stop')
+  ]
+  curl_form(url, new_resource[:user], new_resource[:password], fields)
+end
+
+def curl_uninstall(new_resource)
+  url = "http://#{new_resource[:host]}:#{new_resource[:port]}/system/console/bundles/#{new_resource[:symbolic_name]}"
+  fields = [
+    Curl::PostField.content('action', 'uninstall')
+  ]
+  curl_form(url, new_resource[:user], new_resource[:password], fields)
+end
+
+def bundle?(new_resource, check_version = true, valid_states = nil)
+  url = "http://#{new_resource[:host]}:#{new_resource[:port]}/system/console/bundles/#{new_resource[:symbolic_name]}"
+  c = curl(url, new_resource[:user], new_resource[:password])
+  case c.response_code
+  when 200, 201
+    content = JSON.parse(c.body_str)
+    result =
+      content['data']
+        .select { |h| h['symbolicName'] == new_resource[:symbolic_name] }
+
+    if check_version
+      result =
+        result.select { |h| h['version'] == new_resource[:version] }
+    end
+
+    if valid_states && !valid_states.empty?
+      result =
+        result.select { |h| valid_states.include?(h['state']) }
+    end
+
+    result.size > 0
+  when 404
+    false
+  else
+    fail "Unable to read bundle data at #{url}. response_code: #{c.response_code} response: #{c.body_str}"
+  end
+end
+
+action :install do
+  unless bundle?(new_resource)
+    file_path = "#{Chef::Config[:file_cache_path]}/#{new_resource[:symbolic_name]}-#{new_resource[:nme].jar}"
+    remote_file file_path do
+      source new_resource[:bundle_url]
+    end
+
+    curl_install(file_path, new_resource)
+  end
+end
+
+action :start do
+  unless bundle?(new_resource, false, %w(Active Fragment))
+    curl_activate(new_resource)
+  end
+end
+
+action :stop do
+  if bundle?(new_resource, false)
+    curl_disable(new_resource)
+  end
+end
+
+action :uninstall do
+  if bundle?(new_resource, false)
+    curl_uninstall(new_resource)
+  end
+end
---
+++ resources/bundle.rb
@@ -0,0 +1,30 @@
+#
+# Cookbook Name:: aem
+# Resource:: bundle
+#
+# Copyright 2015
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This resource manages AEM bundles
+
+actions :install, :start, :stop, :uninstall
+
+attribute :symbolic_name, :kind_of => String, :name_attribute => true, :required => true
+attribute :version, :kind_of => String, :default => nil
+attribute :bundle_url, :kind_of => String, :default => nil
+
+attribute :host, :kind_of => String, :default => 'localhost'
+attribute :user, :kind_of => String, :required => true
+attribute :password, :kind_of => String, :required => true
+attribute :port, :kind_of => String, :required => true
