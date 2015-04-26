396cca0270921fea7eb9a2d92cad850462c4ae8d
Tobias L. Maier <me@tobiasmaier.info>
2015-04-26 13:51:22 +0200

    Fix variable name

--- libraries/curl.rb
+++ libraries/curl.rb
@@ -5,8 +5,8 @@ module AEM
     def curl(url)
       c = ::Curl::Easy.new(url.to_s)
       c.http_auth_types = :basic
-      c.username = u.user
-      c.password = u.password
+      c.username = url.user
+      c.password = url.password
       if block_given?
         yield(c)
       else
