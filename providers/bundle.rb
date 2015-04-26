4de5489e2d0df5913286ae27089f77671b55cbf0
Tobias L. Maier <me@tobiasmaier.info>
2015-04-26 15:49:48 +0200

    Replace return with if statement

--- providers/bundle.rb
+++ providers/bundle.rb
@@ -23,57 +23,57 @@ def whyrun_supported?
 end
 
 action :install do
-  return if bundle.installed?
+  unless bundle.installed?
+    file_path = "#{Chef::Config[:file_cache_path]}/#{new_resource.symbolic_name}-#{new_resource.version}.jar"
 
-  file_path = "#{Chef::Config[:file_cache_path]}/#{new_resource.symbolic_name}-#{new_resource.version}.jar"
+    converge_by "Install bundle #{new_resource.symbolic_name}" do
+      remote_file file_path do
+        source new_resource.bundle_url
+        notifies :run, "ruby_block[Install bundle #{new_resource.symbolic_name}]", :immediately
+      end
 
-  converge_by "Install bundle #{new_resource.symbolic_name}" do
-    remote_file file_path do
-      source new_resource.bundle_url
-      notifies :run, "ruby_block[Install bundle #{new_resource.symbolic_name}]", :immediately
-    end
-
-    ruby_block "Install bundle #{new_resource.symbolic_name}" do
-      action :nothing
-      block do
-        bundle.bundle_path = file_path
-        bundle.install!
+      ruby_block "Install bundle #{new_resource.symbolic_name}" do
+        action :nothing
+        block do
+          bundle.bundle_path = file_path
+          bundle.install!
+        end
       end
     end
   end
 end
 
 action :start do
-  return if bundle.started?
-
-  converge_by "Start bundle #{new_resource.symbolic_name}" do
-    ruby_block "Start bundle #{new_resource.symbolic_name}" do
-      block do
-        bundle.start!
+  unless bundle.started?
+    converge_by "Start bundle #{new_resource.symbolic_name}" do
+      ruby_block "Start bundle #{new_resource.symbolic_name}" do
+        block do
+          bundle.start!
+        end
       end
     end
   end
 end
 
 action :stop do
-  return if bundle.stopped?
-
-  converge_by "Stop bundle #{new_resource.symbolic_name}" do
-    ruby_block "Stop bundle #{new_resource.symbolic_name}" do
-      block do
-        bundle.stop!
+  unless bundle.stopped?
+    converge_by "Stop bundle #{new_resource.symbolic_name}" do
+      ruby_block "Stop bundle #{new_resource.symbolic_name}" do
+        block do
+          bundle.stop!
+        end
       end
     end
   end
 end
 
 action :uninstall do
-  return if bundle.uninstalled?
-
-  converge_by "Uninstall bundle #{new_resource.symbolic_name}" do
-    ruby_block "Uninstall bundle #{new_resource.symbolic_name}" do
-      block do
-        bundle.uninstall
+  unless bundle.uninstalled?
+    converge_by "Uninstall bundle #{new_resource.symbolic_name}" do
+      ruby_block "Uninstall bundle #{new_resource.symbolic_name}" do
+        block do
+          bundle.uninstall
+        end
       end
     end
   end
